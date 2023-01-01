b5507a94ece9294f732001952912e3e0f5be0992
HaramPark <haram1117@khu.ac.kr>
2023-01-01 16:23:42 +0900

    DP1699

--- DynamicProgramming/DP1699.cpp
+++ DynamicProgramming/DP1699.cpp
@@ -5,29 +5,36 @@
 #include <iostream>
 using namespace std;
 
-int dp[100000];
+int dp[100000]; // dp 배열 초기화
 
 int main(){
 
+    // 입력
     int N;
     cin >> N;
 
-    dp[1] = 1;
-    for(int i = 2; i <= N; i++){
+    // 각 수를 1^2만으로 더했을 때의 항의 개수를 배열에 저장
+    for(int i = 1; i <= N; i++){
         dp[i] = i;
     }
 
+
     for(int i = 2; i <= N; i++){
+
+        // i보다 작은 제곱수부터 최소 항의 개수 계산
         int temp = 2;
         while(true){
+            // 제곱수가 i보다 크면 반복 종료
             if(i < temp * temp)
                 break;
-            else if(i == temp * temp)
+            else if(i == temp * temp) // i가 제곱수가 되면 dp[i] = 1
                 dp[i] = 1;
+            // dp[i]에 현재 항의 개수와 i - temp * temp의 항의 개수 + 1 중 min 값 저장
             dp[i] = min(dp[i], dp[i - temp * temp] + 1);
             temp++;
         }
     }
+    // 출력
     cout << dp[N];
     return 0;
 }
