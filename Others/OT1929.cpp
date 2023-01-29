ae8c2c1c72c04995aa028dec1ab3c3405e479db1
HaramPark <haram1117@khu.ac.kr>
2023-01-29 11:53:51 +0900

    1929

---
+++ Others/OT1929.cpp
@@ -0,0 +1,34 @@
+//
+// Created by haram on 2023-01-29.
+//
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 소수 구하기
+ * 루트 n 보다 작은 자연수들로 나눠봤을때, 나누어 떨어지는 자연수가 없으면 소수다
+ * */
+bool isPrime(int n){
+    if(n < 2)
+        return false;
+    for(int i = 2; i * i <= n; i++){
+        if(n % i == 0)
+            return true;
+    }
+    return false;
+}
+
+int main(){
+    int m, n;
+    cin >> m >> n;
+
+    for(int i = m; i <= n; i++){
+        if(i == 1)
+            continue;
+        if(!isPrime(i))
+            cout << i << "\n";
+    }
+    return 0;
+}
