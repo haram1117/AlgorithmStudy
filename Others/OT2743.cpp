51f66b46e1a8ffa994f0ecd2b5ba7572f1d94cd8
HaramPark <haram1117@khu.ac.kr>
2023-01-30 13:51:05 +0900

    2743

---
+++ Others/OT2743.cpp
@@ -0,0 +1,17 @@
+//
+// Created by haram on 2023-01-30.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+
+    cout << str.length();
+
+    return 0;
+}
