b4026622af5b88b70d59ded11479db387a6943e4
HaramPark <haram1117@khu.ac.kr>
2023-02-04 19:10:45 +0900

    OT10872

--- Others/OT10872.cpp
+++ Others/OT10872.cpp
@@ -1,3 +1,18 @@
 //
 // Created by haram on 2023-02-04.
 //
+#include <iostream>
+
+using namespace std;
+
+long long factorial(int x){
+    if(x <= 1)
+        return 1;
+    return x * factorial(x - 1);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << factorial(n);
+}
