e37fe92df371eb7827136c4308b64323374ba2e6
HaramPark <haram1117@khu.ac.kr>
2023-03-15 16:40:05 +0900

    10172

---
+++ Class1/10172.cpp
@@ -0,0 +1,11 @@
+//
+// Created by haram on 2023-03-15.
+//
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|\n";
+    return 0;
+}
