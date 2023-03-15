0f2b4f969aaa0ae735df7b8eb8b6c9239e2cd8d8
HaramPark <haram1117@khu.ac.kr>
2023-03-15 14:55:27 +0900

    10869

---
+++ Class1/10869.cpp
@@ -0,0 +1,12 @@
+//
+// Created by haram on 2023-03-15.
+//
+#include <iostream>
+
+using namespace std;
+int a, b;
+int main(){
+    cin >> a >> b;
+    cout << a + b << '\n' << a - b << '\n' << a * b << '\n' << a / b << '\n' << a % b << '\n';
+    return 0;
+}
