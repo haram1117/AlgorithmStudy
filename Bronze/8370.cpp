040a06abe4821d75d1db43d78b21c1acf94e3ce6
HaramPark <haram1117@khu.ac.kr>
2023-07-05 19:33:11 +0900

    8370

---
+++ Bronze/8370.cpp
@@ -0,0 +1,15 @@
+//
+// Created by haram on 2023-07-05
+//
+// Problem-Solving #8370
+
+#include <iostream>
+
+using namespace std;
+int n1, n2, k1, k2;
+
+int main(){
+    cin >> n1 >> k1 >> n2 >> k2;
+    cout << n1 * k1 + n2 * k2;
+    return 0;
+}
