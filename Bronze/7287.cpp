7df7487e80e7a554891047875465ba0db22264a6
HaramPark <haram1117@khu.ac.kr>
2023-07-05 19:28:41 +0900

    7287

---
+++ Bronze/7287.cpp
@@ -0,0 +1,13 @@
+//
+// Created by haram on 2023-07-05
+//
+// Problem-Solving #7287
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << 276 << '\n' << "haram518303";
+    return 0;
+}
