9c4d6cea97954b91de969a26b2834d7a7e73dc66
HaramPark <haram1117@khu.ac.kr>
2023-07-05 19:45:16 +0900

    9654

---
+++ Bronze/9654.cpp
@@ -0,0 +1,18 @@
+//
+// Created by haram on 2023-07-05
+//
+// Problem-Solving #9654
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n";
+    cout << "N2 Bomber      Heavy Fighter  Limited    21        \n";
+    cout << "J-Type 327     Light Combat   Unlimited  1         \n";
+    cout << "NX Cruiser     Medium Fighter Limited    18        \n";
+    cout << "N1 Starfighter Medium Fighter Unlimited  25        \n";
+    cout << "Royal Cruiser  Light Combat   Limited    4         ";
+    return 0;
+}
