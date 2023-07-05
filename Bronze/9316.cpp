d5fee3264a3f8ec4770b3d16b1c6dfe6965d4e78
HaramPark <haram1117@khu.ac.kr>
2023-07-05 19:37:56 +0900

    9316

---
+++ Bronze/9316.cpp
@@ -0,0 +1,15 @@
+//
+// Created by haram on 2023-07-05
+//
+// Problem-Solving #9316
+
+#include <iostream>
+
+using namespace std;
+int n;
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; ++i)
+        cout << "Hello World, Judge " << i + 1 << "!\n";
+    return 0;
+}
