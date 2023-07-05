5eace0c2d212dec50a73dec32f2a90f4dcb00dc2
HaramPark <haram1117@khu.ac.kr>
2023-07-05 19:30:42 +0900

    7891

---
+++ Bronze/7891.cpp
@@ -0,0 +1,19 @@
+//
+// Created by haram on 2023-07-05
+//
+// Problem-Solving #7891
+
+#include <iostream>
+
+using namespace std;
+int tc;
+int a, b;
+
+int main(){
+    cin >> tc;
+    for(int i = 0; i < tc; ++i){
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+    return 0;
+}
