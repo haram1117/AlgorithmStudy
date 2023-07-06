e94b87e95b2dd5f960110b98cc5f7d836394a3ee
HaramPark <haram1117@khu.ac.kr>
2023-07-06 17:03:35 +0900

    10926

---
+++ Bronze/10926.cpp
@@ -0,0 +1,16 @@
+//
+// Created by haram on 2023-07-06
+//
+// Problem-Solving #10926
+
+#include <iostream>
+
+using namespace std;
+string str;
+int main(){
+    cin >> str;
+
+    cout << str << "??!";
+
+    return 0;
+}
