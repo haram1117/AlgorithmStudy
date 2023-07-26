50ee0ac2ac60043fa201b114f658c61aebf01268
HaramPark <haram1117@khu.ac.kr>
2023-07-26 14:28:53 +0900

    9655

---
+++ 9655.cpp
@@ -0,0 +1,15 @@
+//
+// Created by haram on 2023-07-26
+//
+// Problem-Solving #9655
+
+#include <iostream>
+
+using namespace std;
+int n;
+
+int main(){
+    cin >> n;
+    cout << (n % 2 ? "SK" : "CY");
+    return 0;
+}
