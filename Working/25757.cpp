bfa9a7d2d2c67dff477bc4c58d7cb92dc59cb548
HaramPark <haram1117@khu.ac.kr>
2023-08-07 15:12:16 +0900

    25757

---
+++ Working/25757.cpp
@@ -0,0 +1,15 @@
+//
+// Created by haram on 2023-08-04
+//
+// Problem-Solving #25757
+
+#include <iostream>
+
+using namespace std;
+int a;
+int main(){
+    cin >> a;
+    cout << "HI";
+    cout << "aaaasassdfaaaaaasdf";
+    return 0;
+}
