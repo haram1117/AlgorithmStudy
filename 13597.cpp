e6844aacb3fc66f9b818013e26688b0316719dab
HaramPark <haram1117@khu.ac.kr>
2023-08-20 23:43:23 +0900

    13597

---
+++ 13597.cpp
@@ -0,0 +1,13 @@
+//
+// Created by haram on 2023-08-20
+//
+// Problem-Solving #13597
+#include <iostream>
+
+using namespace std;
+int a, b;
+int main(){
+    cin >> a >> b;
+    cout << max(a, b);
+    return 0;
+}
