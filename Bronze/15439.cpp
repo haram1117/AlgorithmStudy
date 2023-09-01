875116a22f1a6d9ce3d1fb2995757a4673af9f54
HaramPark <haram1117@khu.ac.kr>
2023-09-01 16:02:44 +0900

    15439

---
+++ Bronze/15439.cpp
@@ -0,0 +1,13 @@
+//
+// Created by haram on 2023-09-01
+//
+// Problem-Solving #15439
+#include <iostream>
+
+using namespace std;
+int n;
+int main(){
+    cin >> n;
+    cout << n * (n - 1);
+    return 0;
+}
