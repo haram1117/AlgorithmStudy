12de80fe803c9fc96e5d0c173820208f744f89c6
HaramPark <haram1117@khu.ac.kr>
2023-09-01 16:03:59 +0900

    15680

---
+++ Bronze/15680.cpp
@@ -0,0 +1,16 @@
+//
+// Created by haram on 2023-09-01
+//
+// Problem-Solving #15680
+#include <iostream>
+
+using namespace std;
+int n;
+int main(){
+    cin >> n;
+    if(n == 0)
+        cout << "YONSEI";
+    else
+        cout << "Leading the Way to the Future";
+    return 0;
+}
