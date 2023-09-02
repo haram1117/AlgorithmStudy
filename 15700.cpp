4bcd6e154607144b276cdd150b725c818e345a65
HaramPark <haram1117@khu.ac.kr>
2023-09-02 12:59:19 +0900

    15700

---
+++ 15700.cpp
@@ -0,0 +1,13 @@
+//
+// Created by haram on 2023-09-02
+//
+// Problem-Solving #15700
+#include <iostream>
+
+using namespace std;
+unsigned long long n, m;
+int main(){
+    cin >> n >> m;
+    cout << (n * m) / 2;
+    return 0;
+}
