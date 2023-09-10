cbfdd56a98e8e75a216278f56133ed8fef58dc79
HaramPark <haram1117@khu.ac.kr>
2023-09-10 11:34:07 +0900

    18108

---
+++ Bronze/18108.cpp
@@ -0,0 +1,13 @@
+//
+// Created by haram on 2023-09-10
+//
+// Problem-Solving #18108
+#include <iostream>
+
+using namespace std;
+int y;
+int main(){
+    cin >> y;
+    cout << y - 543;
+    return 0;
+}
