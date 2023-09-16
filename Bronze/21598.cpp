4527b9f57f5df8dce7d34417996dc6707bd48928
HaramPark <haram1117@khu.ac.kr>
2023-09-16 11:31:19 +0900

    21598

---
+++ Bronze/21598.cpp
@@ -0,0 +1,14 @@
+//
+// Created by haram on 2023-09-16
+//
+// Problem-Solving #21598
+#include <iostream>
+
+using namespace std;
+int n;
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; ++i)
+        cout << "SciComLove\n";
+    return 0;
+}
