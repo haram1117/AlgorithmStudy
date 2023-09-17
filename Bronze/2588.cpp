90495ec6b186c114f2db8b71daff0be0d542fd45
HaramPark <haram1117@khu.ac.kr>
2023-09-17 21:23:42 +0900

    2588

---
+++ Bronze/2588.cpp
@@ -0,0 +1,14 @@
+//
+// Created by haram on 2023-09-17
+//
+// Problem-Solving #2588
+#include <iostream>
+typedef unsigned long long ull;
+using namespace std;
+
+ull a, b, c;
+int main(){
+    cin >> a >> b >> c;
+    cout << a + b + c;
+    return 0;
+}
